af22a7686ab08321999c3dd010ea105a0ca957b6
KimJangHyeon <rlawkdgus75@gmail.com>
2018-07-05 12:34:19 +0900

    all done before setting database

---
+++ django_database/appDB/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UserDetail
+
+urlpatterns = [
+	path('', UserDetail.as_view()),
+	]
--- django_database/appDB/views.py
+++ django_database/appDB/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import User 
+
+from .serializers import UserSerializer
 
 # Create your views here.
+class UserDetail(APIView):
+	def get(self, request):
+		users = User.objects.all()
+		serializer = UserSerializer(users, many=True)
+		return Response(serializer.data)
